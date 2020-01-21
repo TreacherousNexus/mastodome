53770a607ab1c717ce673cc46a51d88f92a974f3
Bobby Moss <45732696+TrechNex@users.noreply.github.com>
2020-01-21 20:49:25 +0000

    Update config.py

--- config/config.py
+++ config/config.py
@@ -11,5 +11,5 @@ class Config:
         self.GUI_DEFAULTS_FILE_LOC_PREFIX = "/config/.defaults/"
         self.GUI_TOOT_MAX_SIZE_CHARS = 280
         self.APP_NAME = "Mastodome"
-        self.APP_VERSION = "0.3.5"
+        self.APP_VERSION = "0.3.6"
         self.APP_WEBSITE = "https://github.com/TreacherousNexus/mastodome-legacy"
